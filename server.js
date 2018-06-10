f7d987638ea2a4e8aa9610dcf282e00bf69a9a09
HugoCattelain <hugo.cattelain@outlook.com>
2018-06-10 16:32:18 +0200

    fix: router forward

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.set("port", process.env.PORT || 3001);
